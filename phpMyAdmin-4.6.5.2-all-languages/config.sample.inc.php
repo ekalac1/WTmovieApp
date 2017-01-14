2f395af9f970f2c481ee9babbab564a77315f459
ekalac1 <elzaa_95@hotmail.com>
2017-01-14 23:45:52 +0100

    Stres

--- phpMyAdmin-4.6.5.2-all-languages/config.sample.inc.php
+++ phpMyAdmin-4.6.5.2-all-languages/config.sample.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['Servers'][$i]['host'] = getenv('MYSQL_SERVICE_HOST'); /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['host'] = getenv('MYSQL_SERVICE_HOST');
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
